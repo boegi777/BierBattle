e085e0a6856f45d513991625b51cdd68bf9e9040
JuriBog <juri.bogatyrjow@stud.hs-hannover.de>
2017-12-26 16:50:00 +0100

    Scanner

--- app/src/main/java/com/fantavier/bierbattle/bierbattle/MenueTab.java
+++ app/src/main/java/com/fantavier/bierbattle/bierbattle/MenueTab.java
@@ -10,22 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
-/**
- * Created by Mir on 14.12.2017.
- */
+
 
 public class MenueTab extends Fragment {
 
     private static final String TAG = "MenueTab";
     private DatabaseReference usersRef;
-    private ImageButton kamera_btn;
-
     public TextView username;
 
     @Override
@@ -33,22 +26,6 @@ public class MenueTab extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.menue_tab, container, false);
         ImageButton zurueck = (ImageButton) rootView.findViewById(R.id.bier);
-        kamera_btn = (ImageButton) rootView.findViewById(R.id.kamera);
-        final Activity activity = getActivity();
-        kamera_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(false);
-                integrator.setBarcodeImageEnabled(false);
-                integrator.initiateScan();
-                integrator.setOrientationLocked(false);
-            }
-        });
-
         try {
             username = (TextView) rootView.findViewById(R.id.username);
             if(MainActivity.userProvider == null){
@@ -98,24 +75,6 @@ public class MenueTab extends Fragment {
             Log.d(TAG, e.getMessage());
         }
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode,data);
-        if(result != null){
-            if(result.getContents()== null){
-                Toast.makeText(getActivity(),"You cancelled the scanning",Toast.LENGTH_LONG).show();
-            }
-            else{
-                Toast.makeText(getActivity(),result.getContents(),Toast.LENGTH_LONG).show();
-            }
-        }
-        else{
-
-            super.onActivityResult(requestCode,resultCode,data);
-        }
-
-    }
-
 
 }
 
